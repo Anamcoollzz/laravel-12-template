eb06c3db0ea774cd86375dd182f81b6265d0a716
Anamcoollzz <hairulanam21@gmail.com>
2025-08-22 01:52:09 +0700

    update js global

--- resources/views/stisla/includes/others/js-auth.blade.php
+++ resources/views/stisla/includes/others/js-auth.blade.php
@@ -39,3 +39,10 @@
 @if (config('app.env') == 'local' && env('LIVERELOAD'))
   <script src="http://localhost:35729/livereload.js"></script>
 @endif
+
+<script>
+  $('form').on('submit', function(event) {
+    $(this).find('button').addClass('btn-progress disabled');
+    $(this).find('.btn-secondary').addClass('btn-progress disabled');
+  });
+</script>
