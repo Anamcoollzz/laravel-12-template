1a569fa88a780d8cb70083033ecdcf643bc0be33
Anamcoollzz <hairulanam21@gmail.com>
2025-11-09 23:21:28 +0700

    update repository

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,30 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\AppEnum;
-use App\Models\ActivityLog;
-use App\Models\Bank;
-use App\Models\BankDeposit;
-use App\Models\BankDepositHistory;
-use App\Models\CrudExample;
-use App\Models\Faculty;
-use App\Models\LogRequest;
-use App\Models\Menu;
-use App\Models\MenuGroup;
-use App\Models\Notification;
-use App\Models\PermissionGroup;
-use App\Models\User;
 use App\Repositories\SettingRepository;
-use App\Services\DatabaseService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Spatie\Permission\Models\Permission;
-use App\Models\Role;
-use App\Models\Student;
-use App\Models\StudyProgram;
+use App\Repositories\DashboardRepository;
 
 class DashboardController extends StislaController
 {
+
+    private DashboardRepository $dashboardRepository;
 
     /**
      * constructor method
@@ -35,6 +20,8 @@ class DashboardController extends StislaController
     public function __construct()
     {
         parent::__construct();
+
+        $this->dashboardRepository = new DashboardRepository;
 
         // $this->middleware('can:Log Aktivitas');
     }
@@ -46,255 +33,10 @@ class DashboardController extends StislaController
      */
     public function index()
     {
-        $widgets = [];
-        $user = auth_user();
-
-        if ($user->can('Contoh CRUD'))
-            $widgets[] = (object)[
-                'title' => 'Contoh CRUD',
-                'count' => CrudExample::count(),
-                'bg'    => 'primary',
-                'icon'  => 'atom',
-                'route' => route('crud-examples.index'),
-            ];
-        if ($user->can('Bank'))
-            $widgets[] = (object)[
-                'title' => 'Bank',
-                'count' => Bank::count(),
-                'bg'    => 'primary',
-                'icon'  => 'university',
-                'route' => route('banks.index'),
-                'bg_color' => 'lightblue'
-            ];
-        if ($user->can('Deposito Bank'))
-            $widgets[] = (object)[
-                'title' => 'Deposito Bank',
-                'count' => BankDeposit::count(),
-                'bg'    => 'primary',
-                'icon'  => 'dollar',
-                'route' => route('bank-deposits.index'),
-                'bg_color' => 'lightgreen'
-            ];
-        if ($user->can('Riwayat Deposito Bank'))
-            $widgets[] = (object)[
-                'title' => 'Riwayat Deposito Bank',
-                'count' => BankDepositHistory::count(),
-                'bg'    => 'primary',
-                'icon'  => 'dollar',
-                'route' => route('bank-deposit-histories.index'),
-                'bg_color' => 'cyan'
-            ];
-        if ($user->can('Riwayat Deposito Bank'))
-            $widgets[] = (object)[
-                'title' => 'Keuntungan Deposito',
-                'count' => rp(BankDepositHistory::sum('realization')),
-                'bg'    => 'primary',
-                'icon'  => 'dollar',
-                'route' => route('bank-deposit-histories.index'),
-                'bg_color' => '#8b743f'
-            ];
-        if ($user->can('Mahasiswa') && !is_mahasiswa())
-            $widgets[] = (object)[
-                'title' => 'Mahasiswa',
-                'count' => is_pimpinan_fakultas() ? Student::query()->whereHas('studyProgram.faculty', function ($q) {
-                    $q->where('id', auth_user()->facultyLeader->faculty->id);
-                })->count() : Student::count(),
-                'bg'    => 'primary',
-                'icon'  => 'users',
-                'route' => route('students.index'),
-                'bg_color' => '#3f6e8bff'
-            ];
-        if ($user->can('Alumni') && !is_mahasiswa())
-            $widgets[] = (object)[
-                'title' => 'Alumni',
-                'count' => is_pimpinan_fakultas() ? Student::query()->whereHas('studyProgram.faculty', function ($q) {
-                    $q->where('id', auth_user()->facultyLeader->faculty->id);
-                })
-                    ->where('student_status', 'lulus')
-                    ->count() : Student::where('student_status', 'lulus')->count(),
-                'bg'    => 'primary',
-                'icon'  => 'users',
-                'route' => route('alumnis.index'),
-                'bg_color' => '#3f8b8bff'
-            ];
-        if ($user->can('Program Studi'))
-            $widgets[] = (object)[
-                'title' => 'Program Studi',
-                'count' => StudyProgram::count(),
-                'bg'    => 'primary',
-                'icon'  => 'book',
-                'route' => route('study-programs.index'),
-                'bg_color' => '#3f8b44ff'
-            ];
-        if ($user->can('Fakultas'))
-            $widgets[] = (object)[
-                'title' => 'Fakultas',
-                'count' => Faculty::count(),
-                'bg'    => 'primary',
-                'icon'  => 'university',
-                'route' => route('faculties.index'),
-                'bg_color' => '#8b3f64ff'
-            ];
-        if ($user->can('Pengguna')) {
-            $widgets[] = (object)[
-                'title' => 'Pengguna',
-                'count' => is_app_chat() ? User::role('user')->count() : User::count(),
-                'bg'    => 'danger',
-                'icon'  => 'users',
-                'route' => route('user-management.users.index'),
-            ];
-            if (is_app_chat()) {
-                $widgets[] = (object)[
-                    'title' => User::GENDER_MALE,
-                    'count' => User::where('gender', User::GENDER_MALE)->role('user')->count(),
-                    'bg'    => 'info',
-                    'icon'  => 'mars',
-                    'route' => route('user-management.users.index'),
-                ];
-                $widgets[] = (object)[
-                    'title' => User::GENDER_FEMALE,
-                    'count' => User::where('gender', User::GENDER_FEMALE)->role('user')->count(),
-                    'bg'    => 'primary',
-                    'icon'  => 'venus',
-                    'route' => route('user-management.users.index'),
-                ];
-                $widgets[] = (object)[
-                    'title' => 'Usia 10-18 Tahun',
-                    'count' => User::age1018()->role('user')->count(),
-                    'bg'    => 'warning',
-                    'icon'  => 'users',
-                    'route' => route('user-management.users.index'),
-                ];
-                $widgets[] = (object)[
-                    'title' => 'Usia 19-25 Tahun',
-                    'count' => User::age1925()->role('user')->count(),
-                    'bg'    => 'warning',
-                    'icon'  => 'users',
-                    'route' => route('user-management.users.index'),
-                ];
-                $widgets[] = (object)[
-                    'title' => 'Usia 26-50 Tahun',
-                    'count' => User::age2650()->role('user')->count(),
-                    'bg'    => 'warning',
-                    'icon'  => 'users',
-                    'route' => route('user-management.users.index'),
-                ];
-                $widgets[] = (object)[
-                    'title' => 'Usia > 51 Tahun',
-                    'count' => User::age511000()->role('user')->count(),
-                    'bg'    => 'warning',
-                    'icon'  => 'users',
-                    'route' => route('user-management.users.index'),
-                ];
-                $widgets[] = (object)[
-                    'title' => 'Orang Majalengka',
-                    'count' => User::where('is_majalengka', 1)->role('user')->count(),
-                    'bg'    => 'danger',
-                    'icon'  => 'users',
-                    'route' => route('user-management.users.index'),
-                ];
-                $widgets[] = (object)[
-                    'title' => 'Orang Bukan Majalengka',
-                    'count' => User::where('is_majalengka', 0)->role('user')->count(),
-                    'bg'    => 'danger',
-                    'icon'  => 'users',
-                    'route' => route('user-management.users.index'),
-                ];
-            }
-        }
-        if ($user->can('Role'))
-            $widgets[] = (object)[
-                'title' => 'Role',
-                'count' => Role::count(),
-                'bg'    => 'success',
-                'icon'  => 'lock',
-                'route' => route('user-management.roles.index')
-            ];
-        if ($user->can('Permission'))
-            $widgets[] = (object)[
-                'title' => 'Permission',
-                'count' => Permission::count(),
-                'bg'    => 'info',
-                'icon'  => 'key',
-                'route' => route('user-management.permissions.index')
-            ];
-        if ($user->can('Group Permission'))
-            $widgets[] = (object)[
-                'title' => 'Group Permission',
-                'count' => PermissionGroup::count(),
-                'bg'    => 'info',
-                'icon'  => 'key',
-                'route' => route('user-management.permission-groups.index'),
-                'bg_color' => 'brown'
-            ];
-        if ($user->can('Menu'))
-            $widgets[] = (object)[
-                'title' => 'Menu',
-                'count' => Menu::count(),
-                'bg'    => 'primary',
-                'icon'  => 'bars',
-                'route' => route('menu-managements.index'),
-                'bg_color' => 'pink'
-            ];
-        if ($user->can('Grup Menu'))
-            $widgets[] = (object)[
-                'title' => 'Grup Menu',
-                'count' => MenuGroup::count(),
-                'bg'    => 'danger',
-                'icon'  => 'bars',
-                'route' => route('menu-managements.index'),
-                'bg_color' => 'orange'
-            ];
-        if ($user->can('Notifikasi')) {
-            $widgets[] = (object)[
-                'title' => 'Notifikasi',
-                'count' => Notification::where('user_id', $user->id)->count(),
-                'bg'    => 'info',
-                'icon'  => 'bell',
-                'route' => route('notifications.index'),
-                'bg_color' => 'navy'
-            ];
-        }
-        if ($user->can('Log Aktivitas'))
-            $widgets[] = (object)[
-                'title' => 'Log Aktivitas',
-                'count' => ActivityLog::count(),
-                'bg'    => 'success',
-                'icon'  => 'clock-rotate-left',
-                'route' => route('activity-logs.index'),
-                'bg_color' => 'black'
-            ];
-        if ($user->can('Log Request'))
-            $widgets[] = (object)[
-                'title' => 'Log Request',
-                'count' => LogRequest::count(),
-                'bg'    => 'success',
-                'icon'  => 'clock-rotate-left',
-                'route' => route('request-logs.index'),
-                'bg_color' => '#C88A65',
-            ];
-        if ($user->can('Pengaturan'))
-            $widgets[] = (object)[
-                'title' => 'Pengaturan',
-                'count' => '6',
-                'bg'    => 'success',
-                'icon'  => 'cogs',
-                'route' => route('settings.all'),
-                'bg_color' => '#E9D66B',
-            ];
-        if ($user->can('Backup Database')) {
-            $widgets[] = (object)[
-                'title' => 'Backup Database',
-                'count' => count((new DatabaseService)->getAllBackupMysql()),
-                'bg'    => 'primary',
-                'icon'  => 'database',
-                'route' => route('backup-databases.index'),
-                'bg_color' => 'purple',
-            ];
-        }
-
-        $logs = $this->activityLogRepository->getMineLatest();
-        $is_chat = config('stisla.app') === AppEnum::APP_CHAT && is_user();
+        $user    = auth_user();
+        $widgets = $this->dashboardRepository->getWidgets();
+        $logs    = $this->activityLogRepository->getMineLatest();
+        $is_chat = is_app_chat() && is_user();
 
         // \Debugbar::enable();
         // \Debugbar::disable();
@@ -307,6 +49,12 @@ class DashboardController extends StislaController
         ]);
     }
 
+    /**
+     * upload file
+     *
+     * @param Request $request
+     * @return Response
+     */
     public function post(Request $request)
     {
         $request->validate([
@@ -324,8 +72,10 @@ class DashboardController extends StislaController
      */
     public function home()
     {
-        return view('welcome-chat2');
-        return view('welcome-chat');
+        if (is_app_chat()) {
+            return view('welcome-chat2');
+            return view('welcome-chat');
+        }
         return view('stisla.homes.index', [
             'title' => __('Selamat datang di ') . SettingRepository::applicationName(),
         ]);
